8021f8a141f53b2ddcfc8daabb5bb6e3bcbcc3a1
anonymous anonymous <not4get2read@yandex>
2018-11-09 04:20:21 +0200

    something

--- git-remote-add-off.sh
+++
@@ -1,2 +0,0 @@
-git remote set-url --push off https://github.com/i4get2code/operaco-md.git
-git remote set-url --add --push off ../operaco-md.git
--- git-remote-add-on.sh
+++
@@ -1,3 +0,0 @@
-git remote set-url on https://github.com/i4get2code/operaco-md.git
-git remote set-url --push on ../operaco-md.git
-git remote set-url --add --push on https://github.com/i4get2code/operaco-md.git
